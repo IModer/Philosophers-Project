72b0119479ff81e746e569ce9414978f016cdc82
IModer <34884631+IModer@users.noreply.github.com>
2023-03-26 14:45:41 +0200

    Updated main.cpp

--- Source/App/main.cpp
+++ Source/App/main.cpp
@@ -1,8 +1,18 @@
 #include "global.h"
+#include "App.h"
+
+//TODO remove this
 #include "../Model/Game.h"
 
 int main()
 {
+    App* _app = App::Instance();
+    _app->Run();
+    //Here we can handle things that should happen after the game is closed
+
+    // This should be View code
+    //  |
+    //  V
     InitWindow(400, 400, "test");
     SetTargetFPS(60);
 
