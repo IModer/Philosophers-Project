e56bd8728b160d3602339dfb2bbe7e0c1a4fcd18
Fegyó Benedek <pz20tk@inf.elte.hu>
2023-03-26 20:04:07 +0000

    Upload New File

---
+++ model/persistence.h
@@ -0,0 +1,13 @@
+#ifndef PERSISTANCE_H_DEFINED
+#define PERSISTANCE_H_DEFINED
+#include "gameObject.h"
+#include <list>
+
+class Persistence
+{
+public:
+    std::list<GameObject> readGameState(std::string path);
+    bool writeGameState(std::string path, std::list<GameObject> fields);
+};
+
+#endif
