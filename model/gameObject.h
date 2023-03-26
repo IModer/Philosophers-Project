362892b8d95a88f4177180a7bd58fd176ab89a85
Fegyó Benedek <pz20tk@inf.elte.hu>
2023-03-26 20:03:35 +0000

    Upload New File

---
+++ model/gameObject.h
@@ -0,0 +1,15 @@
+#ifndef GAME_OBJECT_H_DEFINED
+#define GAME_OBJECT_H_DEFINED
+
+#include<iostream>
+
+
+class GameObject
+{
+    public:
+        std::string toString();
+    private:
+};
+
+
+#endif
