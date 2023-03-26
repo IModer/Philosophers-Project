64dabfc697be80c2a2703d2cd8931f1208c2fa42
Horváth Botond Solt <p9o7ki@INF.ELTE.HU>
2023-03-26 21:26:32 +0200

    Added View

---
+++ Source/View/View.h
@@ -0,0 +1,15 @@
+#ifndef _VIEW_
+#define _VIEW_
+
+#include "../App/global.h"
+// #include "../Model/Model.h" // még nincs
+
+class View 
+{
+    public:
+    void Render();
+    private:
+    // Model _model;
+};
+
+#endif
