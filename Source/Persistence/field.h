44b7f280a741c8524a013333caec3dd71bb819f4
Horváth Botond Solt <p9o7ki@INF.ELTE.HU>
2023-04-03 01:28:44 +0200

    Field implementation

--- Source/Persistence/field.h
+++ Source/Persistence/field.h
@@ -3,17 +3,26 @@
 
 #include "../App/global.h"
 #include "gameObject.h"
+#include <raylib.h>
 
 
 class Field : public GameObject
 {
     public:
+        // Constructor
         Field(int id, INT_TOUPLE location, INT_TOUPLE size): id(id), location(location), size(size) {}
+        // Get, set
+        int GetX()  { return location.x; }
+        int GetY() { return location.y; }
+        // INT_TOUPLE GetPos(); // ha kell
+        int GetWidth() { return size.x; }
+        int GetHeight() { return size.y; }
+        // Functions
         std::string toString();
-
         void Render();
     
     protected:
+        // Variables
         int id;
         INT_TOUPLE location;
         INT_TOUPLE size;
