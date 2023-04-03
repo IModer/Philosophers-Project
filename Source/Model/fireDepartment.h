96535b33a7295ba51b5d3eb7740fef5d99211c2f
Horváth Botond Solt <p9o7ki@INF.ELTE.HU>
2023-04-03 09:04:13 +0200

    Renamed GameField

--- Source/Model/fireDepartment.h
+++ Source/Model/fireDepartment.h
@@ -24,11 +24,11 @@ public:
 
     std::string toString() 
     {
-        return  std::to_string(id) + " " +
-                std::to_string(location.getX()) + " " +
-                std::to_string(location.getY()) + " " +
-                std::to_string(size.getX()) + " " +
-                std::to_string(size.getY());
+        return  STR(id) + " " +
+                STR(location.x) + " " +
+                STR(location.y) + " " +
+                STR(size.x) + " " +
+                STR(size.y);
     }
 };
 
