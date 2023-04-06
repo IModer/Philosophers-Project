99b4c21faa2980b23a85c52c29a80054db743bec
IModer <34884631+IModer@users.noreply.github.com>
2023-04-06 13:51:22 +0200

    writeGameState of Persistence

--- Source/Persistence/Persistence.cpp
+++ Source/Persistence/Persistence.cpp
@@ -1,20 +1,47 @@
 #include "Persistence.h"
+#include <iostream>
+#include <fstream>
 
-std::list<GameObject> Persistence::readGameState(std::string path)
+/**
+    *   \brief Reads the contents of path and returns it.
+    * 
+    *   \param path The path to the file to write to
+    *   \return List of GameObjects read from a file at path
+    **/
+std::list<GameObject*> Persistence::readGameState(std::string path)
 {
-    auto l = new std::list<GameObject>;
+    auto l = std::list<GameObject*>();
     //Open file at path
     //try reading from it, parse into l
     
     return l;
 }
 
-bool Persistence::writeGameState(std::string path, std::list<GameObject> fields)
+/**
+    *   \brief Writes the contents of fields to path.
+    *   Calls .toString() on all items of fields
+    *   
+    *   \warning Overwrites whatever was in path  
+    * 
+    *   \param path The path to the file to write to
+    *   \param fields list of GameObjects to write
+    *   \return True if it was successful
+    **/
+bool Persistence::writeGameState(std::string path, std::list<GameObject*> fields)
 {
     //Open file at path
-    //try writing fields to the file
-
-    return false; //unsuccessful
+    std::ofstream f;
+    f.open(path, std::ios::out | std::ios::trunc);
+    if (f.is_open())
+    {
+        //try writing fields to the file
+        for (auto field : fields)
+        {
+            f << field->toString() << std::endl;
+        }
+    } else {
+        return false; //unsuccessful
+    }
 
     return true; //successful
 }
