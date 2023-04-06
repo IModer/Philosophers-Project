98a8595e214bd3fb0ff2caefb60ff51d3017f8b7
IModer <34884631+IModer@users.noreply.github.com>
2023-04-06 12:44:24 +0200

    Added Persistence.cpp

---
+++ Source/Persistence/Persistence.cpp
@@ -0,0 +1,20 @@
+#include "Persistence.h"
+
+std::list<GameObject> Persistence::readGameState(std::string path)
+{
+    auto l = new std::list<GameObject>;
+    //Open file at path
+    //try reading from it, parse into l
+    
+    return l;
+}
+
+bool Persistence::writeGameState(std::string path, std::list<GameObject> fields)
+{
+    //Open file at path
+    //try writing fields to the file
+
+    return false; //unsuccessful
+
+    return true; //successful
+}
