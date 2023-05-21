9cf12dead45c7c95e7b95f3b45d38372d81a6aed
Fegyó Benedek <pz20tk@inf.elte.hu>
2023-05-21 23:33:49 +0200

    ctor

--- Source/App/test.cpp
+++ Source/App/test.cpp
@@ -1,5 +1,3 @@
-//#define TEST
-
 #ifdef TEST
 
 #define CATCH_CONFIG_MAIN
@@ -7,44 +5,17 @@
 
 #include "../Model/GameModel.h" 
 
+TEST_CASE("GameModel", "[Constructor]") {
+    Persistence persistence; // Create a Persistence object
+    GameModel gameModel(&persistence); // Create a GameModel object using the Persistence object
 
-TEST_CASE("GameModel Tests", "[GameModel]") {
-    Persistence persistence;  // Create a mock Persistence object for testing
-    GameModel gameModel(&persistence);  // Create an instance of GameModel
-
-    SECTION("Initial Game Model State") {
-        // Verify the initial state of GameModel
-        REQUIRE(gameModel._fields.empty());
-        REQUIRE(gameModel.speedOfTime == NORMAL);
-        REQUIRE(gameModel.Gameover == false);
-        REQUIRE(gameModel.satisfaction == gameModel.StartingSatisfaction);
-        // Add more assertions for other members as needed
-    }
-
-    SECTION("Building Functions") {
-        // Test the Build function
-        SECTION("Build Residential Field") {
-            INT_TOUPLE temp = {0,0};
-            std::vector<INT_TOUPLE>* t_vec;
-            t_vec->push_back(temp);
-            bool result = gameModel.Build(RESIDENTIALZONE, t_vec);
-            REQUIRE(result == true);
-            // Add additional assertions to verify the state after building a residential field
-        }
-
-        SECTION("Build Industrial Field") {
-            INT_TOUPLE temp = {0,0};
-            std::vector<INT_TOUPLE>* t_vec;
-            t_vec->push_back(temp);
-            bool result = gameModel.Build(INDUSTRIALZONE, t_vec);
-            REQUIRE(result == false);
-            // Add additional assertions to verify the state after building an industrial field
-        }
-
-        // Add more test cases for other building functions
-    }
-
-    // Add more test sections to cover other functionality of GameModel
+    // Verify initial values
+    REQUIRE(gameModel._persistence == &persistence);
+    REQUIRE(gameModel._fields.empty());
+    REQUIRE(gameModel.speedOfTime == NORMAL);
+    REQUIRE(gameModel.Gameover == false);
+    REQUIRE(gameModel.satisfaction == gameModel.StartingSatisfaction);
+    // Add more assertions for other members if needed
 }
 
 #endif
